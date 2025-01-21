b4625679a386fcf417fddc3d718bd0ea379ba1e3
iqbalmdd <iqbalmuhammad1290@gmail.com>
2025-01-21 21:08:15 +0700

    [ADD] SignUp Page with validation

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,20 @@ import ProductsPage from './components/Products/ProductsPage'
 import SingleProductPage from './components/SingleProduct/SingleProductPage'
 import CartPage from './components/Cart/CartPage'
 import MyOrder from './components/MyOrder/MyOrderPages'
+import LoginPage from './components/Authentication/LoginPage'
+import SignupPage from './components/Authentication/SignupPage'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar />
-      <HomePage />
+      {/* <HomePage />
       <ProductsPage />
       <SingleProductPage />
       <CartPage />
-      <MyOrder />
+      <MyOrder /> */}
+      {/* <LoginPage /> */}
+      <SignupPage />
     </div>
   )
 }
--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import HeroSection from './HeroSection'
 import iphone from '../../assets/image/iphone-14-pro.webp'
 import mac from '../../assets/image/mac-system-cut.jfif'
 import FeaturedProducts from './FeaturedProducts'
 
+import HeroSection from './HeroSection'
 const HomePage = () => {
   return (
     <div>
         <HeroSection 
           title="Buy Iphone 14 Pro" 
-          subtitle="Experience the power of the latest Iphone 14 with our most Pro camera ever"
+          subtitle="Experience the power of the latest 
+                    Iphone 14 with our most Pro camera ever"
           link="/"
           image={iphone}
         />
@@ -18,7 +19,9 @@ const HomePage = () => {
 
         <HeroSection 
           title="Build the ultimate setup" 
-          subtitle="You can add Studio display and colour-matched Magic accessories to your bag after configure your mac mini"
+          subtitle="You can add Studio display and colour-matched
+                    Magic accessories to your bag after configure 
+                    your mac mini"
           link="/"
           image={mac}
         />
